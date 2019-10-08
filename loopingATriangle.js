e8b9759c27f5246902458accfc5db569b546e3a7
Alex Abushady <aabushady214@gmail.com>
2019-10-08 11:34:22 -0400

    Update loopingATriangle.js
    Wrapped  script in function, and added using strict. Replaced vars for lets.

--- loopingATriangle.js
+++ loopingATriangle.js
@@ -1,27 +1,32 @@
-/*
-Write a loop that makes seven calls to console.log to output the following triangle:
+/*global console*/
+/*jshint esversion:6*/
 
-#
-##
-###
-####
-#####
-######
-#######
-*/
+// Write a loop that makes seven calls to console.log to output the following triangle:
 
-// Constants and Variables
-const loopAmount = 200;
-var originalString = "";
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
 
-// Takes in a string and adds a hash symbol to the end.
-function hashAdd(String) {
-    return originalString += "#";
-}
+(function () {
+    "using strict";
 
-// Loop while loopAmount is less then the length of the string.
-// Outputs the result at the end of each loop.
-for(cnt = originalString.length; cnt < loopAmount; cnt++) {
-    originalString = hashAdd(originalString);
-    console.log(originalString);
-}
+    // Constants and Variables
+    const loopAmount = 7;
+    let originalString = "";
+
+    // Takes in a string and adds a hash symbol to the end.
+    function hashAdd(String) {
+        return originalString += "#";
+    }
+
+    // Loop while loopAmount is less then the length of the string.
+    // Outputs the result at the end of each loop.
+    for (let cnt = originalString.length; cnt < loopAmount; cnt++) {
+        originalString = hashAdd(originalString);
+        console.log(originalString);
+    }
+})();
